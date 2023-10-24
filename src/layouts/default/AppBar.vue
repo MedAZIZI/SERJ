dff12188cac04328dbcf22a0e6e2b080f9977d94
Mohammed AZIZI <65093603+MedAZIZI@users.noreply.github.com>
2023-10-24 20:48:36 +0200

    Add Project

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+      <v-img height="100" width="100" src="@/assets/logo serj bg.png" />
+        SERJ
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
